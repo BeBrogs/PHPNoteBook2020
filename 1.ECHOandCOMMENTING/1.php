339d0e6b255609b8fd0699b41510d67936200c5d
BeBrogs <60266496+BeBrogs@users.noreply.github.com>
2020-03-04 15:55:40 +0000

    Add files via upload

---
+++ 1.ECHOandCOMMENTING/1.php
@@ -0,0 +1,19 @@
+<?php
+//Welcome to my FIRST PHP project
+//We NEED to end most of our PHP statements with a semi colon, or we will receive an error
+
+
+
+echo "This is how we print a string \n"; 
+print "This is another way to print a string \n";
+
+
+//e.g: Single line Comment
+
+/*e.g.:        Multi
+            Line
+Comment*/
+
+
+
+
---
+++ 1.ECHOandCOMMENTING/echoAndCommentingTut.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "HEllo there! \n";
+print "My name is Brogs\n";
+
+//This is a single line comment
+
+/*This 
+    Is
+        A   
+        Multi line comment */
